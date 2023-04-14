6dd274a43f88d1302d3bb118d220b6f1ca320029
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2023-04-14 10:29:31 +0800

    Update InputFromKeyboard.java

--- Lab01/src/InputFromKeyboard.java
+++ Lab01/src/InputFromKeyboard.java
@@ -1,4 +1,20 @@
-
+import java.util.Scanner;
 public class InputFromKeyboard {
+	public static void main(String args[]) {
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("What is your name?");
+		String strName = keyboard.nextLine();
 
+		System.out.println("How old are you?");
+		int iAge = keyboard.nextInt();
+
+		System.out.println("How tall are you (meter)?");
+		double dHeight = keyboard.nextDouble();
+		
+		System.out.println(
+				"Mr/Ms. " + strName + ", " + 
+				iAge + " years old.\n" +
+				"Your height is " + dHeight + "m.");
+	}
 }
