86024f53ccb8f9472e24fb8b951950e92e20861b
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2023-05-12 08:54:53 +0800

    done passing parameters

---
+++ Lab02/AimsProject/src/aims/TestPassingParameter.java
@@ -0,0 +1,51 @@
+package aims;
+
+public class TestPassingParameter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+		
+		swap(jungleDVD, cinderellaDVD);
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+		System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+		
+		changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+	}
+	
+	
+	// OLD
+//	public static void swap(Object o1, Object o2) {
+//		Object tmp = o1;
+//		o1 = o2;
+//		o2 = tmp;
+//	}
+
+
+	// NEW
+	public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {		
+		String title = dvd1.getTitle();
+		String category = dvd1.getCategory();
+		String director = dvd1.getDirector();
+		int length = dvd1.getLength();
+		float cost = dvd1.getCost();
+		
+		dvd1.setAll(
+			dvd2.getTitle(), 
+			dvd2.getCategory(), 
+			dvd2.getDirector(),
+			dvd2.getLength(),
+			dvd2.getCost()
+		);
+		
+		dvd2.setAll(title, category, director, length, cost);
+	}
+
+	public static void changeTitle(DigitalVideoDisc dvd, String title) {
+		String oldTitle = dvd.getTitle();
+		dvd.setTitle(title);
+		dvd = new DigitalVideoDisc(oldTitle);
+	}
+}
